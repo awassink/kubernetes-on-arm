710dec2fdb0dc4c1e1e70a1bc8901f74bdcd7d9a
Arjen Wassink <awassink@quintor.nl>
2016-05-17 23:59:10 +0200

    upgraded k8s versions

--- images/version.sh
+++ images/version.sh
@@ -5,12 +5,12 @@
 
 ## GITHUB PROJECTS ##
 # These projects are downloaded from "github.com/user/repo/archive/$VERSION.tar.gz"
-K8S_VERSION='v1.2.0'
+K8S_VERSION='v1.2.4'
 ETCD_VERSION='v2.2.5'
 FLANNEL_VERSION='v0.5.5'
 REGISTRY_VERSION='v2.3.1'
-HEAPSTER_VERSION='v1.0.0'
-INFLUXDB_VERSION='v0.10.3'
+HEAPSTER_VERSION='v1.0.2'
+INFLUXDB_VERSION='v0.12.1'
 GRAFANA_VERSION='v2.6.0'
 
 ## OTHER PROJECTS ##
@@ -18,4 +18,4 @@ GRAFANA_VERSION='v2.6.0'
 # Because building with go1.5.1 is 2-4x slower than 1.4.3
 # And there's no big difference in the result
 # TODO: use prebuilt official go1.6
-GO_VERSION='1.4.3'
+GO_VERSION='1.6'
